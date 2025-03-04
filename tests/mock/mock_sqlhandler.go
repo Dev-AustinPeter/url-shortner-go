fd5e8f3ecbe877fa20888dd35bc90a2a8bacd349
Dev-AustinPeter <austin.peter.rodrigues@gmail.com>
2025-03-04 15:40:22 +0000

    remove commented code

--- tests/mock/mock_sqlhandler.go
+++ tests/mock/mock_sqlhandler.go
@@ -46,38 +46,3 @@ func NewMockDB() (*MockDB, sqlmock.Sqlmock) {
 		db:   db,
 	}, mock
 }
-
-/* type MockUrlRepository struct {
-	*MockSqlHandler
-}
-
-func (m *MockUrlRepository) CreateUrl(url string) (*string, error) {
-	args := m.Called(url)
-	return args.Get(0).(*string), args.Error(1)
-}
-
-func (m *MockUrlRepository) GetUrl(shortCode string) (repository.Url, error) {
-	args := m.Called(shortCode)
-	return args.Get(0).(repository.Url), args.Error(1)
-}
-
-func (m *MockUrlRepository) CreateTaskId() (*types.Task, error) {
-	args := m.Called()
-	return args.Get(0).(*types.Task), args.Error(1)
-}
-
-func (m *MockUrlRepository) GetTask(taskId string) (types.Task, error) {
-	args := m.Called(taskId)
-	return args.Get(0).(types.Task), args.Error(1)
-}
-
-func (m *MockUrlRepository) UpdateTask(taskId string, status string, result json.RawMessage) error {
-	args := m.Called(taskId, status, result)
-	return args.Error(0)
-}
-
-func (m *MockUrlRepository) GetAllUrls() ([]repository.Url, error) {
-	args := m.Called()
-	return args.Get(0).([]repository.Url), args.Error(1)
-}
-*/
